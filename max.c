c0c9c05954b90bb0a6b670ab0a8723c499970361
sabarigiri2005 <sabarim5678@gmail.com>
2024-11-25 22:00:28 +0530

    Create max.c

---
+++ max.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    char ch;
+    char str[100];
+    char sentence[100];
+    
+    scanf("%c", &ch);
+    scanf("%s", str);
+    scanf("\n");
+    scanf("%[^\n]%*c", sentence);
+    
+    printf("%c\n", ch);
+    printf("%s\n", str);
+    printf("%s\n", sentence);
+    
+    return 0;
+}
