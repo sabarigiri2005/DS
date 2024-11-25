2179b45d1f67cf9b9fb1257d54a3d86dec190d0c
sabarigiri2005 <sabarim5678@gmail.com>
2024-11-25 22:02:31 +0530

    Create function.c

---
+++ function.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int max(int x, int y) {
+    return (x > y) ? x : y;
+}
+
+int max_of_four(int a, int b, int c, int d) {
+    return max(max(a, b), max(c, d));
+}
+
+int main() {
+    int a, b, c, d;
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+    printf("%d", max_of_four(a, b, c, d));
+    return 0;
+}
